a608272486a9d4e7614692f8e76ad39b2df87185
Kuanysh Stikeyev <skuanysh24@gmail.com>
2024-07-17 23:30:21 +0200

    Game: result page added

---
+++ SvoyaIgra/SvoyaIgra.Game/ViewModels/Helpers/ScoreRelativeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using SvoyaIgra.Game.Metadata;
+
+namespace SvoyaIgra.Game.ViewModels.Helpers
+{
+    public class ScoreRelativeConverter : IMultiValueConverter
+    {
+        public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var score = (int)values[0];
+
+            var players = (ObservableCollection<Player>)values[1];
+            var maxScore = players.Max(x => x.Score);
+
+            if (maxScore == 0) return 0.0;
+
+            var maxRelative = int.Parse(parameter.ToString()!) ;
+            
+            var result = score*maxRelative/maxScore;
+            return System.Convert.ToDouble(result);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+            //throw new NotImplementedException();
+        }
+    }
+}
+
